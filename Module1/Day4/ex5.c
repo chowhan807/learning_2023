30a18813e3c66d5b565aa76a8dcd1e5e8ddad8bf
chowhan807 <suryachowhan57@gmail.com>
2023-06-19 19:10:59 +0530

    commit 4 : main : Added Module1/Day4/

---
+++ Module1/Day4/ex5.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+void evenOddDiff(int arr[], int size){
+    int even=0, odd=0;
+    for (int i=0; i<size; i++){
+        if (i%2==0) even+=arr[i];
+        else odd+=arr[i];
+    }
+    if (even > odd)
+        printf("difference between even and odd indexed elements of array is : %d\n",even-odd);
+    else
+        printf("difference between even and odd indexed elements of array is : %d\n", odd-even);
+    return;
+}
+int main(){
+    int l;
+    printf("Enter the Size of Array : ");
+    scanf("%d",&l);
+    int arr[l];
+    printf("Enter Array elements : ");
+    for (int i=0; i<l; i++){
+        scanf("%d",&arr[i]);
+    }
+    evenOddDiff(arr,l);
+    return 0;
+}
